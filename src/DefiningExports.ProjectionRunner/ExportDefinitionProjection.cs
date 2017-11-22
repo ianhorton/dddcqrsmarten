76a05d57b8ec0f8a6f9e97d11d334dc068bf89e1
IRH <ian.horton@ticketmaster.co.uk>
2017-11-22 15:45:52 +0000

    shit

---
+++ src/DefiningExports.ProjectionRunner/ExportDefinitionProjection.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Dapper;
+using Marten;
+using Marten.Events.Projections.Async;
+using DefiningExports.Events;
+using Marten.Events.Projections;
+using Marten.Storage;
+using Npgsql;
+
+namespace DefiningExports.ProjectionRunner
+{
+	public partial class Program
+	{
+		public class ExportDefinitionProjection : IProjection
+		{
+			private readonly string _connectionString;
+
+			public Guid Id { get; set; }
+
+			public Type[] Consumes { get; } = {typeof(ExportDefinitionCreated), typeof(ExportRowAddedToExportDefinition)};
+
+			public AsyncOptions AsyncOptions { get; } = new AsyncOptions();
+
+			public ExportDefinitionProjection(string connectionString)
+			{
+				_connectionString = connectionString;
+			}
+
+			public void Apply(IDocumentSession session, EventPage page)
+			{
+			}
+
+			public async Task ApplyAsync(IDocumentSession session, EventPage page, CancellationToken token)
+			{
+				using (var c = new NpgsqlConnection(_connectionString))
+				{
+					await c.OpenAsync(token).ConfigureAwait(false);
+					using (var t = c.BeginTransaction())
+					{
+						try
+						{
+							var creationEvents = page.Events.OrderBy(s => s.Sequence).Select(s => s.Data)
+								.OfType<ExportDefinitionCreated>();
+							foreach (var e in creationEvents)
+							{
+								await t.Connection.ExecuteAsync(@"
+										INSERT into export_definition (id, title)
+										VALUES (@ExportDefinitionId, @Title);", e);
+							}
+
+							var rowAddedEvents = page.Events.OrderBy(s => s.Sequence).Select(s => s.Data)
+								.OfType<ExportRowAddedToExportDefinition>();
+							foreach (var e in rowAddedEvents)
+							{
+								await t.Connection.ExecuteAsync(@"
+										INSERT into export_row (id, export_definition_id, name)
+										VALUES (@ExportRowId, @ExportDefinitionId, @Name);", e);
+							}
+
+							await t.CommitAsync(token).ConfigureAwait(false);
+
+						}
+						catch
+						{
+							await t.RollbackAsync(token).ConfigureAwait(false);
+							throw;
+						}
+					}
+				}
+			}
+
+			public void EnsureStorageExists(ITenant tenant)
+			{
+
+			}
+		}
+
+	}
+}
--- src/DefiningExports.ProjectionRunner/Program.cs
+++ src/DefiningExports.ProjectionRunner/Program.cs
@@ -1,20 +1,12 @@
 ﻿using System;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using Baseline.Dates;
-using Dapper;
 using Marten;
 using Marten.Events;
 using Marten.Events.Projections.Async;
-using DefiningExports.Events;
-using Marten.Events.Projections;
-using Marten.Storage;
-using Npgsql;
 
 namespace DefiningExports.ProjectionRunner
 {
-	public class Program
+	public partial class Program
 	{
 		static void Main()
 		{
@@ -23,77 +15,14 @@ namespace DefiningExports.ProjectionRunner
 				_.Connection("host=localhost; database=event_store; password=postgres; username=postgres");
 				_.Events.StreamIdentity = StreamIdentity.AsString;
 				_.Events.AsyncProjections.Add(new ExportDefinitionProjection("host=localhost; database=read_model; password=postgres; username=postgres"));
-
 			});
 
 			using (var daemon = store.BuildProjectionDaemon(logger: new ConsoleDaemonLogger(),
 				settings: new DaemonSettings {LeadingEdgeBuffer = 1.Seconds()}))
 			{
 				daemon.StartAll();
+				//daemon.RebuildAll();
 				Console.ReadLine();
-			}
-		}
-
-		public class ExportDefinitionProjection : IProjection
-		{
-			private readonly string _connectionString;
-
-			public Guid Id { get; set; }
-
-			public Type[] Consumes { get; } = {typeof(ExportDefinitionCreated), typeof(ExportRowAddedToExportDefinition)};
-
-			public AsyncOptions AsyncOptions { get; } = new AsyncOptions();
-
-			public ExportDefinitionProjection(string connectionString)
-			{
-				_connectionString = connectionString;
-			}
-
-			public void Apply(IDocumentSession session, EventPage page)
-			{
-			}
-
-			public async Task ApplyAsync(IDocumentSession session, EventPage page, CancellationToken token)
-			{
-				using (var c = new NpgsqlConnection(_connectionString))
-				{
-					await c.OpenAsync(token).ConfigureAwait(false);
-					using (var t = c.BeginTransaction())
-					{
-						try
-						{
-							var creationEvents = page.Events.OrderBy(s => s.Sequence).Select(s => s.Data)
-								.OfType<ExportDefinitionCreated>();
-							foreach (var e in creationEvents)
-							{
-								await t.Connection.ExecuteAsync(@"
-										INSERT into export_definition (id, title)
-										VALUES (@ExportDefinitionId, @Title);", e);
-							}
-
-							var rowAddedEvents = page.Events.OrderBy(s => s.Sequence).Select(s => s.Data)
-								.OfType<ExportRowAddedToExportDefinition>();
-							foreach (var e in rowAddedEvents)
-							{
-								await t.Connection.ExecuteAsync(@"
-										INSERT into export_row (id, export_definition_id, name)
-										VALUES (@ExportRowId, @ExportDefinitionId, @Name);", e);
-							}
-
-							await t.CommitAsync(token).ConfigureAwait(false);
-
-						}
-						catch
-						{
-							await t.RollbackAsync(token).ConfigureAwait(false);
-						}
-					}
-				}
-			}
-
-			public void EnsureStorageExists(ITenant tenant)
-			{
-
 			}
 		}
 
